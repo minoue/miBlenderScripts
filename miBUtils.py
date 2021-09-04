2c87774f30c93c1bb57a1f8fb479b7f355c172ce
minoue <michitaka.inoue@icloud.com>
2021-09-04 21:38:36 +0100

    Keep child transform

--- miBUtils.py
+++ miBUtils.py
@@ -1,10 +1,12 @@
+# https://blender.stackexchange.com/questions/9200/how-to-make-object-a-a-parent-of-object-b-via-blenders-python-api
+
 import bpy
 
 bl_info = {
     "name": "mi_blender_utils",
     "description": "small utilies",
     "author": "Michitaka Inoue",
-    "version": (0, 1, 1),
+    "version": (0, 1, 2),
     "blender": (2, 80, 0),
     "warning": "",
     "wiki_url": "",
@@ -22,18 +24,28 @@ class MIU_OT_maya_group(bpy.types.Operator):
         sel = bpy.context.selected_objects
 
         if sel:
+            # Craete new empty
             bpy.ops.object.empty_add(type='PLAIN_AXES')
-            loc = bpy.context.selected_objects[0]
+            empty = bpy.context.selected_objects[0]
+            
+            # 
             parents = set([i.parent for i in sel])
-            parent = sel[0].parent
 
+            bpy.context.view_layer.objects.active = empty
+
+            # Parent selected objects to the newly crated empty plain axis
             for i in sel:
-                i.parent = loc
-                i.matrix_parent_inverse = loc.matrix_world.inverted()
+                i.select_set(True)
+                bpy.ops.object.parent_set(keep_transform=True)
 
+            # If selected objects are located in a same hierarchy, parent the empty axis back 
+            # to the original parent
             if len(parents) == 1:
-                loc.parent = list(parents)[0]
-
+                grandparent = list(parents)[0]
+                if grandparent is not None:
+                    empty.parent = grandparent
+                    empty.matrix_parent_inverse = grandparent.matrix_world.inverted()
+                
         return {'FINISHED'}
 
 
