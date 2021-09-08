ca7233d32d5ab1d34960796aae89b918f6ec8ec0
minoue <michitaka.inoue@icloud.com>
2021-09-08 09:05:52 +0100

    Remove group command and add hotkey

--- miBUtils.py
+++ miBUtils.py
@@ -1,52 +1,15 @@
-# https://blender.stackexchange.com/questions/9200/how-to-make-object-a-a-parent-of-object-b-via-blenders-python-api
-
 import bpy
 
 bl_info = {
-    "name": "mi_blender_utils",
-    "description": "small utilies",
+    "name": "toggle_subsurf",
+    "description": "toggle and control subsurf modifier",
     "author": "Michitaka Inoue",
-    "version": (0, 1, 2),
+    "version": (0, 1, 0),
     "blender": (2, 80, 0),
     "warning": "",
     "wiki_url": "",
     "category": ""
     }
-
-
-class MIU_OT_maya_group(bpy.types.Operator):
-    bl_idname = "object.miu_ot_maya_group"
-    bl_label = "Group"
-    bl_options = {'UNDO'}
-
-    def execute(self, context):
-
-        sel = bpy.context.selected_objects
-
-        if sel:
-            # Craete new empty
-            bpy.ops.object.empty_add(type='PLAIN_AXES')
-            empty = bpy.context.selected_objects[0]
-            
-            # 
-            parents = set([i.parent for i in sel])
-
-            bpy.context.view_layer.objects.active = empty
-
-            # Parent selected objects to the newly crated empty plain axis
-            for i in sel:
-                i.select_set(True)
-                bpy.ops.object.parent_set(keep_transform=True)
-
-            # If selected objects are located in a same hierarchy, parent the empty axis back 
-            # to the original parent
-            if len(parents) == 1:
-                grandparent = list(parents)[0]
-                if grandparent is not None:
-                    empty.parent = grandparent
-                    empty.matrix_parent_inverse = grandparent.matrix_world.inverted()
-                
-        return {'FINISHED'}
 
 
 class MIU_OT_higher_subdiv(bpy.types.Operator):
@@ -95,20 +58,38 @@ class MIU_OT_lower_subdiv(bpy.types.Operator):
 
 
 classes = (
-    MIU_OT_maya_group,
     MIU_OT_higher_subdiv,
     MIU_OT_lower_subdiv
 )
+
+addon_keymaps = []
 
 
 def register():
     for cls in classes:
         bpy.utils.register_class(cls)
 
+    wm = bpy.context.window_manager
+    km = wm.keyconfigs.addon.keymaps.new(name='Object Mode', space_type = 'EMPTY')
+    kmi = km.keymap_items.new(MIU_OT_higher_subdiv.bl_idname, 'NUMPAD_PLUS', 'PRESS')
+    kmi2 = km.keymap_items.new(MIU_OT_lower_subdiv.bl_idname, 'NUMPAD_MINUS', 'PRESS')
+
+    addon_keymaps.append((km, kmi))
+    addon_keymaps.append((km, kmi2))
+
 
 def unregister():
     for cls in classes:
         bpy.utils.unregister_class(cls)
+
+    wm = bpy.context.window_manager
+    kc = wm.keyconfigs.addon
+    km = kc.keymaps['Object Mode']
+
+    for km, kmi, in addon_keymaps:
+        km.keymap_items.remove(kmi)
+
+    addon_keymaps.clear()
 
 
 if __name__ == "__main__":
